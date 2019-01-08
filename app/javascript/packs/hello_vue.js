b6906888354990a36371d985883b4525fbfc1901
Daichi Yanagi <michaelyanagi@gmail.com>
2019-01-09 06:00:19 +0900

    Vue.js Components in Rails Views (until 8:30)

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -12,61 +12,63 @@ import App from '../app.vue'
 Vue.component('app', App)
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Vue.js Components in Rails Views
   const app = new Vue({
     el: '[data-behaviour="vue"]',
   })
 
-  // var app = new Vue({
-  //   el: '#app',
-  //   data: {
-  //     message: 'Hello Vue!'
-  //   }
-  // })
-  //
-  //
-  // var app2 = new Vue({
-  //   el: '#app-2',
-  //   data: {
-  //     message: 'You loaded this page on ' + new Date().toLocaleString()
-  //   }
-  // })
-  //
-  // var app3 = new Vue({
-  //   el: '#app-3',
-  //   data: {
-  //     seen: true
-  //   }
-  // })
-  //
-  // var app4 = new Vue({
-  //   el: '#app-4',
-  //   data: {
-  //     todos: [
-  //       { text: 'Learn JavaScript' },
-  //       { text: 'Learn Vue' },
-  //       { text: 'Build something awesome' }
-  //     ]
-  //   }
-  // })
-  //
-  // var app5 = new Vue({
-  //   el: '#app-5',
-  //   data: {
-  //     message: 'Hello Vue.js!'
-  //   },
-  //   methods: {
-  //     reverseMessage: function () {
-  //       this.message = this.message.split('').reverse().join('')
-  //     }
-  //   }
-  // })
-  //
-  // var app6 = new Vue({
-  //   el: '#app-6',
-  //   data: {
-  //     message: 'Hello Vue!'
-  //   }
-  // })
+  // USING RAILS AND VUE JS, PART 1
+  var app1 = new Vue({
+    el: '#app-1',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+
+
+  var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+  })
+
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  })
+
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  })
+
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  })
+
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
 })
 // import Vue from 'vue'
 // import App from '../app.vue'
